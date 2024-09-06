877d07637aab04e28ce40a9a633f919d263fc122
Desy <desypuspita685@gmail.com>
2024-09-06 15:50:32 +0700

    commit desy

--- belajarcollabgithub.java
+++ belajarcollabgithub.java
@@ -1,9 +1,7 @@
 public class belajarcollabgithub {
-    
     public static void main(String[] args) {
         System.out.println("Nama   : Desy Dwi p");
         System.out.println("NIM    : 244107060145");
         System.out.println("Alamat : Simbar");
-        System.out.println("-----------------------------------------------");
     }
 }
